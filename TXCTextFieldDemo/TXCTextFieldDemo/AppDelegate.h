e920b852b0ea9a391e787af7f1596bd6ea331b27
zhangxiong <xtz_pioneer@163.com>
2017-11-09 17:55:09 +0800

    优化项目结构，解决描述字体不能垂直居中问题。

---
+++ TXCTextFieldDemo/TXCTextFieldDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TXCTextFieldDemo
+//
+//  Created by komlin on 2017/11/8.
+//  Copyright © 2017年 张雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TXCTextFieldDemo/TXCTextFieldDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TXCTextFieldDemo
+//
+//  Created by komlin on 2017/11/8.
+//  Copyright © 2017年 张雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
